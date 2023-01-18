2a229fba9934ceee0d9ec34f555baa70875e7844
Douglas Dean <dcdeaniii@Douglass-MacBook-Pro.local>
2023-01-17 23:00:41 -0600

    Modifying coregistration to anatomy

--- core/dmri/workflows/register_to_anat.py
+++ core/dmri/workflows/register_to_anat.py
@@ -137,9 +137,9 @@ def register_to_anat(dwi_image, working_dir, coreg_to_anat = True, T1_image=None
                                          output_matrix  = fsl_transform,
                                          method         = 'FSL',
                                          dof            = dof,
-                                         flirt_options =  ' -cost normmi -interp sinc -searchrx -180 180 -searchry -180 180 -searchrz -180 180 -usesqform')
+                                         flirt_options =  ' -cost normmi -interp sinc -searchrx -180 180 -searchry -180 180 -searchrz -180 180')
 
-                    bbr_options = ' -cost bbr -wmseg ' + WM_Seg._get_filename() + ' -schedule $FSLDIR/etc/flirtsch/bbr.sch -interp sinc -bbrtype global_abs -bbrslope 0.25 -finesearch 18 -init ' + fsl_transform + ' -usesqform'
+                    bbr_options = ' -cost bbr -wmseg ' + WM_Seg._get_filename() + ' -schedule $FSLDIR/etc/flirtsch/bbr.sch -interp sinc -bbrtype global_abs -bbrslope 0.25 -finesearch 18 -init ' + fsl_transform
 
                     tmp_coreg_img = Image(file = working_dir + '/dwi_coreg.nii.gz')
                     reg_tools.linear_reg(input_img      = mov_img,
