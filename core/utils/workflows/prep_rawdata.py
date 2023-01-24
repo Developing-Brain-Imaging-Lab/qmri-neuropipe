42286558871acacb47c90e14c4cd154aeb041273
Douglas Dean <dcdeaniii@Douglass-MacBook-Pro.local>
2023-01-23 22:04:43 -0600

    Corrections to Synb0-Disco topup path

--- core/utils/workflows/prep_rawdata.py
+++ core/utils/workflows/prep_rawdata.py
@@ -248,8 +248,6 @@ def prep_dwi_rawdata(bids_id, bids_rawdata_dir, bids_derivative_dir, bids_dwi_di
 
     if run_topup or distortion_correction == 'Synb0-Disco':
     
-        topup_base = preprocess_dir + '/topup/' + bids_id + '_desc-Topup'
-        
         if not os.path.exists(topup_base + '_fieldcoef.nii.gz'):
     
             #First going to run eddy and motion-correction to ensure images are aligned prior to estimating fields. Data are only used
@@ -273,7 +271,7 @@ def prep_dwi_rawdata(bids_id, bids_rawdata_dir, bids_derivative_dir, bids_dwi_di
 
             if run_topup:
                 distort_proc.perform_topup(dwi_image    = eddy_img,
-                                           topup_base   = topup_base,
+                                           topup_base   = preprocess_dir + '/topup/' + bids_id + '_desc-Topup',
                                            topup_config = topup_config,
                                            dist_corr    = 'Topup',
                                            verbose=verbose)
@@ -284,7 +282,7 @@ def prep_dwi_rawdata(bids_id, bids_rawdata_dir, bids_derivative_dir, bids_dwi_di
                 distcorr.run_synb0_disco(dwi_img        = eddy_img,
                                          t1w_img        = t1w_img,
                                          t1w_mask       = t1w_mask,
-                                         topup_base     = topup_base,
+                                         topup_base     = preprocess_dir + '/topup/' + bids_id + '_desc-Topup',
                                          topup_config   = topup_config,
                                          nthreads   = 1)
 
