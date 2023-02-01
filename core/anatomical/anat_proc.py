d6f48103fcbbec6d0833a38a14a6f522a6de381b
Douglas Dean <dcdeaniii@Douglass-MacBook-Pro.local>
2023-02-01 15:56:52 -0600

    Adding bbr coregistration between t1w and t2w

--- core/anatomical/anat_proc.py
+++ core/anatomical/anat_proc.py
@@ -309,14 +309,14 @@ class AnatomicalPrepPipeline:
             print(biascorr_t1w._get_filename())
             print(coreg_t2._get_filename())
             
-#            #Apply registration to T2w
-#            reg_tools.apply_transform(input_img     = t2w,
-#                                      reference_img = biascorr_t1w,
-#                                      output_img    = coreg_t2,
-#                                      matrix        = os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat'),
-#                                      method        = 'FSL',
-#                                      flirt_options = '-interp sinc')
-#
+            #Apply registration to T2w
+            reg_tools.apply_transform(input_img     = t2w,
+                                      reference_img = biascorr_t1w,
+                                      output_img    = coreg_t2,
+                                      matrix        = os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat'),
+                                      method        = 'FSL',
+                                      flirt_options = '-interp sinc')
+
 #            os.rename(t1w_brain_mask._get_filename(), brain_mask._get_filename())
 #            os.remove(t1w_masked._get_filename())
 #            os.remove(t2w_masked._get_filename())
