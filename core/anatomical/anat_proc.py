df1d4e97a5d9e28888b490acfb5521717c8d9479
Douglas Dean <dcdeaniii@Douglass-MacBook-Pro.local>
2023-02-01 09:15:36 -0600

    Adding bbr coregistration between t1w and t2w

--- core/anatomical/anat_proc.py
+++ core/anatomical/anat_proc.py
@@ -304,6 +304,7 @@ class AnatomicalPrepPipeline:
                                  flirt_options =  bbr_options)
                                  
             exit()
+                                 
             
 #            tmp_coreg_t2 = Image(file = bids_t2w_derivative_dir +'/tmp_coreg_t2w.nii.gz')
 #            reg_tools.apply_transform(input_img     = t2w,
