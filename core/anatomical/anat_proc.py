9447ca8b1df92a86c764a9134a692ca1a79643fa
Douglas Dean <dcdeaniii@Douglass-MacBook-Pro.local>
2023-02-03 11:44:28 -0600

    Adding bbr coregistration between t1w and t2w

--- core/anatomical/anat_proc.py
+++ core/anatomical/anat_proc.py
@@ -289,10 +289,9 @@ class AnatomicalPrepPipeline:
                 coreg_t2._set_filename(os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.nii.gz'))
                 
                 
-                wmseg_img = seg_tools.create_wmseg(input_img    = biascorr_t2w,
+                wmseg_img = seg_tools.create_wmseg(input_img    = biascorr_t1w,
                                                    output_dir   = os.path.join(bids_derivative_dir,  args.bids_t1w_dir,'wmseg'),
-                                                   brain_mask   = t2w_brain_mask,
-                                                   modality     = t2w)
+                                                   brain_mask   = t1w_brain_mask)
                                        
                 reg_tools.linear_reg(input_img      = biascorr_t2w,
                                      reference_img  = biascorr_t1w,
