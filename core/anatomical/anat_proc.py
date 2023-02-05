f5fda68f5b27596e438fc4f7138c3e5d804d4c12
Douglas Dean <dcdeaniii@Douglass-MacBook-Pro.local>
2023-02-05 13:14:46 -0600

    Edits to T1w and T2w co-registration

--- core/anatomical/anat_proc.py
+++ core/anatomical/anat_proc.py
@@ -11,6 +11,7 @@ import core.anatomical.workflows.preprocess as anat_proc
 
 import core.registration.registration as reg_tools
 import core.segmentation.segmentation as seg_tools
+import core.anatomical.workflows.compute_synthetic as compute_synthetic
 
 
 class AnatomicalPrepPipeline:
@@ -227,12 +228,109 @@ class AnatomicalPrepPipeline:
                                     ref_img              = args.anat_t2w_ants_mask_template,
                                     ref_mask             = args.anat_t2w_ants_mask_template_mask,
                                     antspynet_modality   = args.anat_antspynet_modality)
-
-                
-            #Denoise and Degibbs raw data
+                                    
+                                    
+            #If both images exists, coregister and then denoise and run bias correction
             biascorr_t1w = None
             biascorr_t2w = None
-            if t1w != None:
+            if t1w and t2w:
+                #First create skull-stripped images based on masks created
+                t1w_masked = Image(file=os.path.join(bids_derivative_dir, args.bids_t1w_dir, bids_id+'_desc-T1w-brain.nii.gz'))
+                t2w_masked = Image(file=os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_desc-T2w-brain.nii.gz'))
+                
+                mask.apply_mask(input_img   = t1w,
+                                mask_img    = t1w_brain_mask,
+                                output_img  = t1w_masked)
+                                
+                mask.apply_mask(input_img   = t2w,
+                                mask_img    = t2w_brain_mask,
+                                output_img  = t2w_masked)
+                                
+                                
+                #Create synthetic T2w from the T1w to try to improve registration
+                if args.verbose:
+                    print('Creating Synthetic T2w Image')
+                        
+                syn_t2w = compute_synthetic.compute_synthetic_t2w(input_t1w    = t1w,
+                                                                  output_dir   = os.path.join(bids_derivative_dir, args.bids_t1w_dir, 'syntheticT2w'),
+                                                                  cmd_args     = args)
+                                                                  
+                wmseg_img = seg_tools.create_wmseg(input_img    = syn_t2w,
+                                                   output_dir   = os.path.join(bids_derivative_dir,  args.bids_t1w_dir,'wmseg'),
+                                                   brain_mask   = t1w_brain_mask,
+                                                   modality     = 't2w')
+                                       
+                                
+                #First, create wm segmentation from T1w image
+                coreg_t2 = copy.deepcopy(biascorr_t2w)
+                coreg_t2._set_filename(os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.nii.gz'))
+                
+                reg_tools.linear_reg(input_img      = t2w,
+                                     reference_img  = syn_t2w,
+                                     output_matrix  = os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat'),
+                                     method         = 'FSL',
+                                     dof            = 6,
+                                     flirt_options =  '-interp sinc -searchrx -180 180 -searchry -180 180 -searchrz -180 180')
+
+                bbr_options = ' -cost bbr -wmseg ' + wmseg_img._get_filename() + ' -schedule $FSLDIR/etc/flirtsch/bbr.sch -interp sinc -bbrtype global_abs -bbrslope 0.25 -coarsesearch 30 -finesearch 10 -init ' + os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat -interp sinc')
+
+                reg_tools.linear_reg(input_img      = t2w,
+                                     reference_img  = syn_t2w,
+                                     output_file    = coreg_t2._get_filename(),
+                                     output_matrix  = os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat'),
+                                     method         = 'FSL',
+                                     dof            = 6,
+                                     flirt_options =  bbr_options)
+                
+                #Apply registration to T2w
+                reg_tools.apply_transform(input_img     = t2w,
+                                          reference_img = syn_t2w,
+                                          output_img    = coreg_t2,
+                                          matrix        = os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat'),
+                                          method        = 'FSL',
+                                          flirt_options = '-interp sinc')
+                                                                            
+                os.rename(t1w_brain_mask._get_filename(), brain_mask._get_filename())
+                os.remove(t1w_masked._get_filename())
+                os.remove(t2w_masked._get_filename())
+                os.remove(t2w_brain_mask._get_filename())
+                
+                denoise_t1w = img_proc.denoise_degibbs(img             = t1w,
+                                                       working_dir     = os.path.join(bids_derivative_dir,  args.bids_t1w_dir,''),
+                                                       suffix          = 'T1w',
+                                                       mask_img        = t1w_brain_mask,
+                                                       denoise_method  = args.anat_denoise_method,
+                                                       gibbs_method    = args.anat_gibbs_correction_method,
+                                                       nthreads        = args.nthreads,
+                                                       verbose         = args.verbose)
+
+                biascorr_t1w = img_proc.perform_bias_correction(img         = denoise_t1w,
+                                                                working_dir = os.path.join(bids_derivative_dir,  args.bids_t1w_dir,''),
+                                                                suffix      = 'T1w',
+                                                                mask_img    = t1w_brain_mask,
+                                                                method      = args.anat_biasfield_correction_method,
+                                                                nthreads    = args.nthreads,
+                                                                verbose     = args.verbose)
+                
+                denoise_t2w = img_proc.denoise_degibbs(img             = coreg_t2,
+                                                       working_dir     = os.path.join(bids_derivative_dir, args.bids_t2w_dir,''),
+                                                       suffix          = 'T2w',
+                                                       mask_img        = brain_mask,
+                                                       denoise_method  = args.anat_denoise_method,
+                                                       gibbs_method    = args.anat_gibbs_correction_method,
+                                                       nthreads        = args.nthreads,
+                                                       verbose         = args.verbose)
+
+                biascorr_t2w = img_proc.perform_bias_correction(img         = denoise_t2w,
+                                                                working_dir = os.path.join(bids_derivative_dir,  args.bids_t2w_dir,''),
+                                                                suffix      = 'T2w',
+                                                                mask_img    = brain_mask,
+                                                                method      = args.anat_biasfield_correction_method,
+                                                                nthreads    = args.nthreads,
+                                                                verbose     = args.verbose)
+
+
+            elif t1w:
                 denoise_t1w = img_proc.denoise_degibbs(img             = t1w,
                                                        working_dir     = os.path.join(bids_derivative_dir,  args.bids_t1w_dir,''),
                                                        suffix          = 'T1w',
@@ -250,7 +348,7 @@ class AnatomicalPrepPipeline:
                                                                 nthreads    = args.nthreads,
                                                                 verbose     = args.verbose)
 
-            if t2w != None:
+            elif t2w:
                 denoise_t2w = img_proc.denoise_degibbs(img             = t2w,
                                                        working_dir     = os.path.join(bids_derivative_dir, args.bids_t2w_dir,''),
                                                        suffix          = 'T2w',
@@ -269,81 +367,7 @@ class AnatomicalPrepPipeline:
                                                                 verbose     = args.verbose)
 
             
-            #Coregister the T1w and T2w if both exist
-            if biascorr_t1w and biascorr_t2w:
-                #First create skull-stripped images based on masks created
-                t1w_masked = Image(file=os.path.join(bids_derivative_dir, args.bids_t1w_dir, bids_id+'_desc-T1w-brain.nii.gz'))
-                t2w_masked = Image(file=os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_desc-T2w-brain.nii.gz'))
-                
-                mask.apply_mask(input_img   = biascorr_t1w,
-                                mask_img    = t1w_brain_mask,
-                                output_img  = t1w_masked)
-                                
-                mask.apply_mask(input_img   = biascorr_t2w,
-                                mask_img    = t2w_brain_mask,
-                                output_img  = t2w_masked)
-                                
-                
-                #First, create wm segmentation from T1w image
-                coreg_t2 = copy.deepcopy(biascorr_t2w)
-                coreg_t2._set_filename(os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.nii.gz'))
-                
-                
-                wmseg_img = seg_tools.create_wmseg(input_img    = biascorr_t1w,
-                                                   output_dir   = os.path.join(bids_derivative_dir,  args.bids_t1w_dir,'wmseg'),
-                                                   brain_mask   = t1w_brain_mask)
-                                       
-                reg_tools.linear_reg(input_img      = biascorr_t2w,
-                                     reference_img  = biascorr_t1w,
-                                     output_matrix  = os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat'),
-                                     method         = 'FSL',
-                                     dof            = 6,
-                                     flirt_options =  '-interp sinc -searchrx -180 180 -searchry -180 180 -searchrz -180 180')
 
-                bbr_options = ' -cost bbr -wmseg ' + wmseg_img._get_filename() + ' -schedule $FSLDIR/etc/flirtsch/bbr.sch -interp sinc -bbrtype global_abs -bbrslope 0.25 -coarsesearch 30 -finesearch 10 -init ' + os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat -interp sinc')
-
-                reg_tools.linear_reg(input_img      = biascorr_t2w,
-                                     reference_img  = biascorr_t1w,
-                                     output_file    = coreg_t2._get_filename(),
-                                     output_matrix  = os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat'),
-                                     method         = 'FSL',
-                                     dof            = 6,
-                                     flirt_options =  bbr_options)
-                
-                #Apply registration to T2w
-                reg_tools.apply_transform(input_img     = t2w,
-                                          reference_img = biascorr_t1w,
-                                          output_img    = coreg_t2,
-                                          matrix        = os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat'),
-                                          method        = 'FSL',
-                                          flirt_options = '-interp sinc')
-                                                                            
-                os.rename(t1w_brain_mask._get_filename(), brain_mask._get_filename())
-                os.remove(t1w_masked._get_filename())
-                os.remove(t2w_masked._get_filename())
-                os.remove(t2w_brain_mask._get_filename())
-
-                os.remove(biascorr_t2w._get_filename())
-                os.remove(os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_desc-Denoised_T2w.nii.gz'))
-                os.remove(os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_desc-GibbsRinging_T2w.nii.gz'))
-
-
-                denoise_t2w = img_proc.denoise_degibbs(img             = coreg_t2,
-                                                       working_dir     = os.path.join(bids_derivative_dir, args.bids_t2w_dir,''),
-                                                       suffix          = 'T2w',
-                                                       mask_img        = brain_mask,
-                                                       denoise_method  = args.anat_denoise_method,
-                                                       gibbs_method    = args.anat_gibbs_correction_method,
-                                                       nthreads        = args.nthreads,
-                                                       verbose         = args.verbose)
-
-                biascorr_t2w = img_proc.perform_bias_correction(img         = denoise_t2w,
-                                                                working_dir = os.path.join(bids_derivative_dir,  args.bids_t2w_dir,''),
-                                                                suffix      = 'T2w',
-                                                                mask_img    = brain_mask,
-                                                                method      = args.anat_biasfield_correction_method,
-                                                                nthreads    = args.nthreads,
-                                                                verbose     = args.verbose)
 
           
         if args.anat_t1w_type == 'mp2rage':
