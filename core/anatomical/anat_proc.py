7b44a8b362fe0fb4d03b2e6a1b7bc99316f2dcaf
Douglas Dean <dcdeaniii@Douglass-MacBook-Pro.local>
2023-02-07 09:27:23 -0600

    Edits to T1w and T2w co-registration

--- core/anatomical/anat_proc.py
+++ core/anatomical/anat_proc.py
@@ -298,23 +298,23 @@ class AnatomicalPrepPipeline:
                 coreg_t2._set_filename(os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.nii.gz'))
                 
                 reg_tools.linear_reg(input_img      = biascorr_t2w,
-                                     reference_img  = syn_t2w,
-                                     output_matrix  = os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat'),
-                                     output_file    = coreg_t2._get_filename(),
-                                     method         = 'FSL',
-                                     dof            = 6,
-                                     flirt_options =  '-cost normcorr')
-
-                bbr_options = ' -cost bbr -wmseg ' + wmseg_img._get_filename() + ' -schedule $FSLDIR/etc/flirtsch/bbr.sch -interp sinc -bbrtype global_abs -bbrslope 0.25 -finesearch 15 -init ' + os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat -interp sinc -searchrx -30 30 -searchry -30 30 -searchrz -30 30')
-
-
-                reg_tools.linear_reg(input_img      = biascorr_t2w,
                                      reference_img  = biascorr_t1w,
-                                     output_file    = coreg_t2._get_filename(),
                                      output_matrix  = os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat'),
+                                     output_file    = coreg_t2._get_filename(),
                                      method         = 'FSL',
                                      dof            = 6,
-                                     flirt_options =  bbr_options)
+                                     flirt_options =  '-cost normmi')
+
+#                bbr_options = ' -cost bbr -wmseg ' + wmseg_img._get_filename() + ' -schedule $FSLDIR/etc/flirtsch/bbr.sch -interp sinc -bbrtype global_abs -bbrslope 0.25 -finesearch 15 -init ' + os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat -interp sinc -searchrx -30 30 -searchry -30 30 -searchrz -30 30')
+#
+#
+#                reg_tools.linear_reg(input_img      = biascorr_t2w,
+#                                     reference_img  = biascorr_t1w,
+#                                     output_file    = coreg_t2._get_filename(),
+#                                     output_matrix  = os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat'),
+#                                     method         = 'FSL',
+#                                     dof            = 6,
+#                                     flirt_options =  bbr_options)
 
                 #Apply registration to T2w
                 reg_tools.apply_transform(input_img     = t2w,
