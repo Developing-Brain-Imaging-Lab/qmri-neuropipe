40be7938c1e6aa439afd5ec19bc8704608529953
Douglas Dean <dcdeaniii@Douglass-MacBook-Pro.local>
2023-02-07 08:45:11 -0600

    Edits to T1w and T2w co-registration

--- core/anatomical/anat_proc.py
+++ core/anatomical/anat_proc.py
@@ -305,10 +305,7 @@ class AnatomicalPrepPipeline:
                                      dof            = 6,
                                      flirt_options =  '-cost normcorr')
 
-#                bbr_options = ' -cost bbr -wmseg ' + wmseg_img._get_filename() + ' -schedule $FSLDIR/etc/flirtsch/bbr.sch -interp sinc -bbrtype global_abs -bbrslope 0.25 -coarsesearch 20 -finesearch 10 -init ' + os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat -interp sinc -searchrx -30 30 -searchry -30 30 -searchrz -30 30')
-
-
-                bbr_options = ' -cost bbr -wmseg ' + wmseg_img._get_filename() + ' -interp sinc -bbrtype global_abs -bbrslope 0.25 -coarsesearch 15 -finesearch 10 -init ' + os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat')
+                bbr_options = ' -cost bbr -wmseg ' + wmseg_img._get_filename() + ' -schedule $FSLDIR/etc/flirtsch/bbr.sch -interp sinc -bbrtype global_abs -bbrslope 0.25 -finesearch 15 -init ' + os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat -interp sinc -searchrx -30 30 -searchry -30 30 -searchrz -30 30')
 
 
                 reg_tools.linear_reg(input_img      = biascorr_t2w,
