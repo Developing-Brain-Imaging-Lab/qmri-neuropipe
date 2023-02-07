b315d2c401ebf9c00bce00fc1afb46dced3aa829
Douglas Dean <dcdeaniii@Douglass-MacBook-Pro.local>
2023-02-07 14:28:01 -0600

    Edits to T1w and T2w co-registration

--- core/anatomical/anat_proc.py
+++ core/anatomical/anat_proc.py
@@ -302,7 +302,7 @@ class AnatomicalPrepPipeline:
                                      output_matrix  = os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat'),
                                      output_file    = coreg_t2._get_filename(),
                                      method         = 'FSL',
-                                     dof            = 12,
+                                     dof            = 6,
                                      flirt_options =  '-cost normmi')
 
 #                bbr_options = ' -cost bbr -wmseg ' + wmseg_img._get_filename() + ' -schedule $FSLDIR/etc/flirtsch/bbr.sch -interp sinc -bbrtype global_abs -bbrslope 0.25 -finesearch 15 -init ' + os.path.join(bids_derivative_dir, args.bids_t2w_dir, bids_id+'_space-individual-T1w_T2w.mat -interp sinc -searchrx -30 30 -searchry -30 30 -searchrz -30 30')
