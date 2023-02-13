d3a4f40d61327159a0e1e9e8aeff70d090765743
Douglas Dean <dcdeaniii@Douglass-MacBook-Pro.local>
2023-02-13 13:22:03 -0600

    Removing debugging print statements

--- core/dmri/dmri_proc.py
+++ core/dmri/dmri_proc.py
@@ -431,10 +431,6 @@ class DiffusionProcessingPipeline:
             fmap_image=Image(file = os.path.join(bids_rawdata_dir, 'fmap-dwi', bids_id+'_fieldmap.nii.gz'))
             fmap_ref_image=Image(file = os.path.join(bids_rawdata_dir, 'fmap-dwi', bids_id+'_magnitude.nii.gz'))
             
-        
-        print(t1w._get_filename())
-        print(t2w._get_filename())
-        
             
         #Check if TORTOISE or coregistration anatomy being performed and ensure image exists
         #First determine the anatomical image to use
