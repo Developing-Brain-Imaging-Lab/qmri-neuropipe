a4b00a68e63ff6168a1774fe66084f35b10901f6
Douglas Dean <dcdeaniii@Douglass-MacBook-Pro.local>
2023-09-30 15:59:08 -0600

    Adding scripts for converting diffusion rawdata

---
+++ scripts/bids_dwi_database.py
@@ -0,0 +1,118 @@
+import string, os, sys, subprocess, shutil, time, json, argparse
+import pandas
+import numpy as np
+from glob import glob
+
+class Subject(object):
+    def __init__(self, id, rawdata_id, pepolar0_dcm_dir, pepolar1_dcm_dir):
+        self.id                 = str(id)
+        self.rawdata_id         = str(rawdata_id)
+        self.pepolar0_dcm_dir   = str(pepolar0_dcm_dir)
+        self.pepolar1_dcm_dir   = str(pepolar1_dcm_dir)
+
+    def __str__(self):
+        return("Subject:\n"
+               "  ID = {0}\n"
+               "  RAW_ID = {1}\n"
+               "  DWI_RAWDATA_PEPOLAR0_DIR = {2}\n"
+               "  DWI_RAWDATA_PEPOLAR1_DIR = {3}\n"
+               .format(self.id, self.rawdata_id, self.pepolar0_dcm_dir, self.pepolar1_dcm_dir))
+
+def create_database(workbook_file):
+    workbook = pandas.read_excel(workbook_file, sheet_name=0, engine='openpyxl')
+
+    ids             = workbook['ID']
+    rawdata_ids     = workbook['RAWDATA_ID']
+    
+    pepolar0_dir    = workbook['DWI_RAWDATA_PEPOLAR0_DIR']
+    pepolar1_dir    = workbook['DWI_RAWDATA_PEPOLAR1_DIR']
+        
+
+    database = []
+    for i in range(0,len(ids)):
+        subject = Subject(id                = ids[i],
+                          rawdata_id        = rawdata_ids[i],
+                          pepolar0_dcm_dir  = pepolar0_dir[i],
+                          pepolar1_dcm_dir  = pepolar1_dir[i])
+        database.append(subject)
+
+    return database
+
+
+
+parser = argparse.ArgumentParser(description='Waisman Center Processing for Quantitative MRI Data in BIDS format')
+
+parser.add_argument('--load_json',
+                    type=str,
+                    help='Load settings from file in json format. Command line options are overriden by values in file.',
+                    default=None)
+                    
+parser.add_argument('--bids_dir',
+                    type=str,
+                    help='Load settings from file in json format. Command line options are overriden by values in file.',
+                    default=None)
+                    
+parser.add_argument('--dwi_pepolar0_bvals',
+                    type=str,
+                    help='Load settings from file in json format. Command line options are overriden by values in file.',
+                    default=None)
+                
+parser.add_argument('--dwi_pepolar0_bvecs',
+                    type=str,
+                    help='Load settings from file in json format. Command line options are overriden by values in file.',
+                    default=None)
+                    
+parser.add_argument('--dwi_pepolar1_bvals',
+                    type=str,
+                    help='Load settings from file in json format. Command line options are overriden by values in file.',
+                    default=None)
+                    
+parser.add_argument('--dwi_pepolar1_bvecs',
+                    type=str,
+                    help='Load settings from file in json format. Command line options are overriden by values in file.',
+                    default=None)
+                    
+                    
+parser.add_argument('--workbook_file',
+                    type=str,
+                    help='Load settings from file in json format. Command line options are overriden by values in file.',
+                    default=None)
+
+args, unknown = parser.parse_known_args()
+
+
+if args.load_json:
+    with open(args.load_json, 'rt') as f:
+        t_args = argparse.Namespace()
+        t_dict = vars(t_args)
+        t_dict.update(json.load(f))
+        args, unknown = parser.parse_known_args(namespace=t_args)
+        
+dwi_database = create_database(args.workbook_file)
+
+
+#for i in range(0, len(dwi_database)):
+
+for i in range(1, 2):
+
+    convert_raw_script = 'python convert_dwi_rawdata.py '
+    convert_raw_script += ' --subject=' + str(dwi_database[i].id).zfill(3)
+    convert_raw_script += ' --bids_dir=' + args.bids_dir
+    
+    
+
+    if not pandas.isna(dwi_database[i].pepolar0_dcm_dir):
+        convert_raw_script += ' --dwi_dcm_dir='+str(dwi_database[i].pepolar0_dcm_dir)
+        convert_raw_script += ' --dwi_bvals='+args.dwi_pepolar0_bvals
+        convert_raw_script += ' --dwi_bvecs='+args.dwi_pepolar0_bvecs
+
+    if not dwi_database[i].pepolar1_dcm_dir:
+        convert_raw_script += ' --dwi_pepolar1_dcm_dir='+str(dwi_database[i].pepolar1_dcm_dir)
+        convert_raw_script += ' --dwi_pepolar1_bvals='+args.dwi_pepolar1_bvals
+        convert_raw_script += ' --dwi_pepolar1_bvecs='+args.dwi_pepolar1_bvecs
+
+
+    print('Working on Subject: ' + str(dwi_database[i].id).zfill(3))
+    print()
+    print(convert_raw_script)
+    #os.system(convert_raw_script)
---
+++ scripts/convert_dwi_rawdata.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+import os,sys, shutil, time, argparse, json
+
+from core.utils.io import Image, DWImage
+import core.dicom.dicom_data as rawdata
+import core.dmri.utils.qc as dmri_qc
+
+parser = argparse.ArgumentParser(description='Convert DIFFUSION DICOM Data to NIFTI Data')
+parser.add_argument('--subject', type=str, help='Subject ID')
+parser.add_argument('--session', type=str, help='Subject Timepoint', default=None)
+parser.add_argument('--bids_dir', type=str, help='BIDS Directory')
+parser.add_argument('--nthreads', type=int, help='Number of Threads', default=1)
+parser.add_argument('--nifti_convert_method', type=str, help='Method to use for NIFTI conversion', default="dcm2niix", choices=["dcm2niix", "dcm2nii", "mrtrix", "mri-convert"])
+
+parser.add_argument('--dwi_dcm_dir', type=str, help='Path to DICOM DWI directory')
+parser.add_argument('--dwi_bvals', type=str, help='Path to DWI B-values', default=None)
+parser.add_argument('--dwi_bvecs', type=str, help='Path to DWI B-vectors', default=None)
+
+parser.add_argument('--dwi_pepolar1_dcm_dir', type=str, help='Path to Reversed Phase Encode DWI DICOM directory', default=None)
+parser.add_argument('--dwi_pepolar1_bvals', type=str, help='Path to Reverse Phase Encode B-values', default=None)
+parser.add_argument('--dwi_pepolar1_bvecs', type=str, help='Path to Reverse Phase Encode B-vectors', default=None)
+
+parser.add_argument('--dwi_fieldmap_dcm_dir', type=str, help='Path to DICOM data for B0 Fieldmap', default=None)
+parser.add_argument('--dwi_fieldmap_ref_dcm_dir', type=str, help='Path to DICOM data for B0 Fieldmap reference image', default=None)
+
+args = parser.parse_args()
+
+bids_id=""
+bids_sub_dir=""
+
+if args.session:
+	bids_id                = 'sub-' + args.subject + '_ses-' + args.session.zfill(2)
+	bids_sub_dir           = 'sub-' + args.subject + '/ses-' + args.session.zfill(2)
+else:
+	bids_id                = 'sub-' + args.subject
+	bids_sub_dir           = 'sub-' + args.subject
+
+bids_rawdata_dir       = args.bids_dir + '/rawdata/' + bids_sub_dir + '/'
+
+
+##################################
+##################################
+##### PROCESSING STARTS HERE #####
+##################################
+##################################
+
+bids_dwi_dir    = bids_rawdata_dir + 'dwi/'
+
+if not os.path.exists(bids_dwi_dir):
+    os.makedirs(bids_dwi_dir)
+
+if args.dwi_pepolar1_dcm_dir != None:
+    bids_dwi        = bids_dwi_dir + bids_id + '_desc-pepolar-0_dwi.nii.gz'
+    bids_dwi_json   = bids_dwi_dir + bids_id + '_desc-pepolar-0_dwi.json'
+    bids_dwi_bvals  = bids_dwi_dir + bids_id + '_desc-pepolar-0_dwi.bval'
+    bids_dwi_bvecs  = bids_dwi_dir + bids_id + '_desc-pepolar-0_dwi.bvec'
+else:
+    bids_dwi        = bids_dwi_dir + bids_id + '_dwi.nii.gz'
+    bids_dwi_json   = bids_dwi_dir + bids_id + '_dwi.json'
+    bids_dwi_bvals  = bids_dwi_dir + bids_id + '_dwi.bval'
+    bids_dwi_bvecs  = bids_dwi_dir + bids_id + '_dwi.bvec'
+
+print('Converting DWI Images...')
+rawdata.dicom_to_nifti(dcm_dir     = args.dwi_dcm_dir,
+                       method      = args.nifti_convert_method,
+                       output_img  = bids_dwi)
+if args.dwi_bvals != None:
+    shutil.copy2(args.dwi_bvals, bids_dwi_bvals)
+if args.dwi_bvecs != None:
+    shutil.copy2(args.dwi_bvecs, bids_dwi_bvecs)
+
+#Check to make sure the Numebr of Bvals/Bvecs match acquired data
+dmri_qc.check_bvals_bvecs(DWImage(file = bids_dwi, bvals=bids_dwi_bvals, bvecs=bids_dwi_bvecs))
+
+###CONVERT THE REVERSE PHASE ENCODE DATA ###
+if args.dwi_pepolar1_dcm_dir != None:
+    bids_dwi        = bids_dwi_dir + bids_id + '_desc-pepolar-1_dwi.nii.gz'
+    bids_dwi_json   = bids_dwi_dir + bids_id + '_desc-pepolar-1_dwi.json'
+    bids_dwi_bvals  = bids_dwi_dir + bids_id + '_desc-pepolar-1_dwi.bval'
+    bids_dwi_bvecs  = bids_dwi_dir + bids_id + '_desc-pepolar-1_dwi.bvec'
+
+    print('Converting Reversed-Phase Encode DWI Images...')
+    rawdata.dicom_to_nifti_dcm2niix(dcm_dir     = args.dwi_pepolar1_dcm_dir,
+                                    output_img  = bids_dwi)
+
+    if args.dwi_pepolar1_bvals != None:
+        shutil.copy2(args.dwi_pepolar1_bvals, bids_dwi_bvals)
+    if args.dwi_pepolar1_bvecs != None:
+        shutil.copy2(args.dwi_pepolar1_bvecs, bids_dwi_bvecs)
+
+    #Check to make sure the Numebr of Bvals/Bvecs match acquired data
+    dmri_qc.check_bvals_bvecs(DWImage(file = bids_dwi, bvals=bids_dwi_bvals, bvecs=bids_dwi_bvecs))
+
+
+###CONVERT FMAP DATA IF AVAILABLE
+if args.dwi_fieldmap_dcm_dir != None:
+
+    bids_fmap_dir    = bids_rawdata_dir + 'fmap-dwi/'
+
+    if not os.path.exists(bids_fmap_dir):
+        os.makedirs(bids_fmap_dir)
+
+    bids_fmap       = bids_fmap_dir + bids_id + '_fieldmap.nii.gz'
+    bids_fmap_ref   = bids_fmap_dir + bids_id + '_magnitude.nii.gz'
+
+    print('Converting DWI Fieldmap Images...')
+    rawdata.dicom_to_nifti_dcm2niix(dcm_dir     = args.dwi_fieldmap_dcm_dir,
+                                    output_img  = bids_fmap)
+
+    rawdata.dicom_to_nifti_dcm2niix(dcm_dir     = args.dwi_fieldmap_ref_dcm_dir,
+                                    output_img  = bids_fmap_ref)
+
+    os.remove(bids_fmap_dir + bids_id + '_magnitude.json')
