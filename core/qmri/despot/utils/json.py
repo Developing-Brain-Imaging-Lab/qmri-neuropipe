3505f19cbf9da8ab7d8e768f686c5ac5097100f7
Doug Dean <dcdeaniii@Dougs-MacBook-Pro.local>
2024-01-12 10:39:31 -0600

    DESPOT processing changes

--- core/qmri/despot/utils/json.py
+++ core/qmri/despot/utils/json.py
@@ -9,7 +9,7 @@ def create_processing_json(despot_json, spgr_img, ssfp_img, irspgr_img=None, afi
     if not os.path.exists(despot_json):
 
         if spgr_img != None:
-            with open(spgr_img._get_json(), 'r+') as spgr_file:
+            with open(spgr_img.json, 'r+') as spgr_file:
                 data = json.load(spgr_file)
                 despot_data['SPGR'] = []
                 despot_data['SPGR'].append({
@@ -23,7 +23,7 @@ def create_processing_json(despot_json, spgr_img, ssfp_img, irspgr_img=None, afi
                     json.dump(despot_data, outfile, indent=4, sort_keys=True)
 
         if ssfp_img != None:
-            with open(ssfp_img._get_json(), 'r+') as ssfp_file:
+            with open(ssfp_img.json, 'r+') as ssfp_file:
                 data = json.load(ssfp_file)
                 despot_data['SSFP'] = []
                 despot_data['SSFP'].append({
@@ -37,7 +37,7 @@ def create_processing_json(despot_json, spgr_img, ssfp_img, irspgr_img=None, afi
                     json.dump(despot_data, outfile, indent=4, sort_keys=True)
 
         if irspgr_img != None:
-            with open(irspgr_img._get_json(), 'r+') as irspgr_file:
+            with open(irspgr_img.json, 'r+') as irspgr_file:
                 data = json.load(irspgr_file)
                 despot_data['IRSPGR'] = []
                 despot_data['IRSPGR'].append({
