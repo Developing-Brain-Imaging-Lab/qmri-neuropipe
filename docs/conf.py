096a947d2e58121fc34145b990fd6cdc007cbe10
dcdeaniii <dcdeaniii@me.com>
2024-10-17 11:25:58 -0500

    Add Read the Docs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,11 @@ release = 'v0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
