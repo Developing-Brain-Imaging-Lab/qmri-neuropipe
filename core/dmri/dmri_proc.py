8670c5487253dc232c71ce03559f6d5cc44a6448
Doug Dean III <deaniii@Dougs-MacBook-Pro.local>
2025-07-24 10:01:07 -0500

    Bug fixes

--- core/dmri/dmri_proc.py
+++ core/dmri/dmri_proc.py
@@ -995,7 +995,7 @@ class DiffusionProcessingPipeline:
             if self.preproc['run_topup'] or self.preproc['run_synb0'] or self.opts.dist_correction != None:
                 self.preproc["topup_base"] = os.path.join(self.dmri_preproc_dir, "rawdata", "topup", self.bids_id+"_desc-Topup")
     
-                if not os.path.exists(f"{self.preproc["topup_base"]}_fieldcoef.nii.gz"):
+                if not os.path.exists(f'{self.preproc["topup_base"]}_fieldcoef.nii.gz'):
                     #First going to run eddy_correct in order to perform an initial motion-correction to ensure images are aligned prior to estimating fields. Data are only used
                     #here and not for subsequent processing
                     eddy_img = self.EddyCurrentCorrection(DWI         = DWI,
