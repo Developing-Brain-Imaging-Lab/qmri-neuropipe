9e9b718a9cbde8654af3962e92f9ac6fe15e9396
Doug Dean III <deaniii@Dougs-MacBook-Pro.local>
2025-10-14 13:33:56 -0500

    Adding cropping/padding

--- core/utils/tools.py
+++ core/utils/tools.py
@@ -110,30 +110,13 @@ def crop_or_pad_image(input_img, output_file, target_size, debug=False):
         ystart = int(np.floor(ydiff/2))
         zstart = int(np.floor(zdiff/2))
 
-        CMD = "fslroi " + input_img.filename + " " + tmp_img.filename + " " \
+        CMD = "fslroi " + input_img.filename + " " + output_img.filename + " " \
             + str(max(0,xstart)) + " " + str(target_size[0]) + " " \
             + str(max(0,ystart)) + " " + str(target_size[1]) + " " \
             + str(max(0,zstart)) + " " + str(target_size[2])
 
         subprocess.run([CMD], shell=True, stderr=subprocess.STDOUT)    
 
-        if xdiff < 0 or ydiff < 0 or zdiff < 0:
-            pad_x_before = int(np.ceil(-xdiff/2))
-            pad_y_before = int(np.ceil(-ydiff/2))
-            pad_z_before = int(np.ceil(-zdiff/2))
-            pad_x_after = int(np.floor(-xdiff/2))
-            pad_y_after = int(np.floor(-ydiff/2))
-            pad_z_after = int(np.floor(-zdiff/2))
-
-            CMD = "fslmaths " + tmp_img.filename + " -add 0 -pad " \
-                + str(pad_x_before) + " " + str(pad_x_after) + " " \
-                + str(pad_y_before) + " " + str(pad_y_after) + " " \
-                + str(pad_z_before) + " " + str(pad_z_after) + " " \
-                + output_img.filename
-            subprocess.run([CMD], shell=True, stderr=subprocess.STDOUT)    
-            os.remove(tmp_img.filename)
-        else:
-            os.rename(tmp_img.filename, output_img.filename)
 
     return output_img
         
