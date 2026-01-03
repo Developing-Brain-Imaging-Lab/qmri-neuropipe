d4824b0718b06ed5017e9b40223a38ca7cb5198e
Doug Dean III <deaniii@Dougs-MacBook-Pro.local>
2026-01-03 10:47:25 -0600

    Bug fixes

--- src/qmri_neuropipe/lib/common/registration.py
+++ src/qmri_neuropipe/lib/common/registration.py
@@ -354,7 +354,7 @@ class CoregistrationStep(BaseProcessingStep):
                          moving_for_reg = b0_path
                     
                     # Collect extra options (exclude known args)
-                    known_args = ['dof', 'cost', 'extra_args', 'output_resolution', 'interpolation', 'enabled', 'reference_image', 'method']
+                    known_args = ['dof', 'cost', 'extra_args', 'output_resolution', 'interpolation', 'enabled', 'reference_image', 'method', 'wm_seg_method']
                     fsl_extra_opts = {k: v for k, v in options.items() if k not in known_args}
                     
                     # Calculate transform
