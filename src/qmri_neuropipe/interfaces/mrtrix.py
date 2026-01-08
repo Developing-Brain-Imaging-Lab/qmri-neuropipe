38afdea203ca9e14610e4e554e9130bc7e64120d
Doug Dean III <deaniii@Dougs-MacBook-Pro.local>
2026-01-08 14:39:19 -0600

    Bug fixes

--- src/qmri_neuropipe/interfaces/mrtrix.py
+++ src/qmri_neuropipe/interfaces/mrtrix.py
@@ -537,7 +537,7 @@ def mrtransform(
     if force:
         cmd.append("-force")
     
-    cmd.append(["-strides", str(template)])
+    cmd.extend(["-strides", str(template)])
     cmd.append("-reorient_fod no")
     cmd.append("-quiet")
     
