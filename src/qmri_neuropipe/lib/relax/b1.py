deb8042a5c8f8fe97179e74f0cd614c25f56fd25
Doug Dean III <deaniii@Dougs-MacBook-Pro.local>
2026-01-16 15:40:59 -0600

    Bug fixes

--- src/qmri_neuropipe/lib/relax/b1.py
+++ src/qmri_neuropipe/lib/relax/b1.py
@@ -229,9 +229,6 @@ class B1MappingStep(BaseProcessingStep):
         alpha_act = np.arccos(arg)
         b1_map = alpha_act / flip_angle_rad
         
-        # Clip crazy values
-        #b1_map = np.clip(b1_map, 0, 2.0) # B1 usually 0.5 to 1.5
-        
         # Save
         # Save Intermediate: sub-XX_desc-preproc_TB1AFI
         # Use entities from input
